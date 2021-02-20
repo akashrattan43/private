af446eb374b358fe14e365c26a4464f2ffeda4b7
akashrattan43 <akashrattan43@gmail.com>
2021-02-20 02:02:46 -0800

    finish

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -1,13 +1,13 @@
-const redirect = window.redirect
+const redirect = window.redirect;
 
-const SECRET_COMBO = [1, 3, 5, 1]
+const SECRET_COMBO = [1, 3, 5, 1];
 
 const lockState = window.mobx.observable({
   locked: true,
-  wheels: [0, 0, 0, 0]
-})
+  wheels: [0, 0, 0, 0],
+});
 
-function changeDialValue (index, incrementBy) {
+function changeDialValue(index, incrementBy) {
   // This part is missing some code
   // This function is automatically called when the user clicks on a chevron
   // it will be called with a wheel index and an amount to change the value by
@@ -15,6 +15,13 @@ function changeDialValue (index, incrementBy) {
   // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
   // for example, if the user clicked the "down" arrow for the last wheel
   // this will be called with arguments (3, -1).
+  if (lockState.wheels[index] == 0 && incrementBy == 1){
+    lockState.wheels[index] += incrementBy;
+  } else if(lockState.wheels[index] == 9 && incrementBy == -1){
+    lockState.wheels[index] += incrementBy;
+  } else if (lockState.wheels[index] < 9 && lockState.wheels[index] >= 1){
+    lockState.wheels[index] += incrementBy;
+  }
 
   // to change the state of the lock, simply make a call like
   // lockState.locked = false
@@ -25,8 +32,13 @@ function changeDialValue (index, incrementBy) {
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
+  
+  if (JSON.stringify(lockState.wheels) === JSON.stringify(SECRET_COMBO)){
+    lockState.locked = false;
+    redirect('akashdeep-rattan');
+  }
 }
 
 // let our other modules find our functions
-window.lockState = lockState
-window.changeDialValue = changeDialValue
+window.lockState = lockState;
+window.changeDialValue = changeDialValue;
